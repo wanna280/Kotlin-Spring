80a3e9000c481bb10eb22890871638e08ecd5127
jianchaojia <2801304373@qq.com>
2022-10-02 05:37:54 +0800

    完善PropertySourceLoader

--- Kotlin-Spring-Boot-Project/Kotlin-Spring-Boot/src/main/kotlin/com/wanna/boot/env/YamlPropertySourceLoader.kt
+++ Kotlin-Spring-Boot-Project/Kotlin-Spring-Boot/src/main/kotlin/com/wanna/boot/env/YamlPropertySourceLoader.kt
@@ -1,16 +1,23 @@
 package com.wanna.boot.env
 
 import com.wanna.framework.core.environment.PropertySource
+import com.wanna.framework.core.io.Resource
 
 /**
  * 这是一个Yaml的PropertySourceLoader
+ *
+ * @see PropertySourceLoader
  */
 open class YamlPropertySourceLoader : PropertySourceLoader {
-    override fun getFileExtensions(): Array<String> {
-        return arrayOf("yaml", "yml")
+    override fun getFileExtensions() = arrayOf("yaml", "yml")
+
+    // TODO
+    override fun load(name: String, resource: String): List<PropertySource<*>> {
+        return emptyList()
     }
 
-    override fun load(name: String, resource: String): List<PropertySource<*>> {
+    // TODO
+    override fun load(name: String, resource: Resource): List<PropertySource<*>> {
         return emptyList()
     }
 }
