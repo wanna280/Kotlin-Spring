adb3550e457f53ea15b015f4d8db9ba2c8a1db60
jianchaojia <2801304373@qq.com>
2022-10-16 23:06:37 +0800

    完善注释

--- Kotlin-Spring-Cloud-Project/Kotlin-Spring-Cloud-Nacos/Kotlin-Spring-Cloud-Nacos-Config/src/main/kotlin/com/wanna/cloud/nacos/config/NacosConfigAutoConfiguration.kt
+++ Kotlin-Spring-Cloud-Project/Kotlin-Spring-Cloud-Nacos/Kotlin-Spring-Cloud-Nacos-Config/src/main/kotlin/com/wanna/cloud/nacos/config/NacosConfigAutoConfiguration.kt
@@ -4,11 +4,16 @@ import com.wanna.cloud.nacos.config.refresh.NacosContextRefresher
 import com.wanna.framework.context.annotation.Bean
 import com.wanna.framework.context.annotation.Configuration
 
+/**
+ * Nacos的自动配置类
+ */
 @Configuration(proxyBeanMethods = false)
 open class NacosConfigAutoConfiguration {
 
     /**
      * 获取配置文件当中对于Nacos的配置信息
+     *
+     * @return NacosConfigProperties
      */
     @Bean
     open fun nacosConfigProperties(): NacosConfigProperties {
@@ -17,6 +22,9 @@ open class NacosConfigAutoConfiguration {
 
     /**
      * NacosConfigManager，负责去进行配置文件的获取
+     *
+     * @param nacosConfigProperties Nacos的配置信息
+     * @return NacosConfigManager，提供对于NacosServer的配置文件的加载功能
      */
     @Bean
     open fun nacosConfigManager(nacosConfigProperties: NacosConfigProperties): NacosConfigManager {
@@ -25,6 +33,9 @@ open class NacosConfigAutoConfiguration {
 
     /**
      * Nacos的ContextRefresher，给NacosClient去注册监听器
+     *
+     * @param nacosConfigManager NacosConfigManager
+     * @return NacosContextRefresher(为所有的NacosPropertySource对应的文件去添加监听器，当对应的文件发生变更时，自动发布RefreshEvent事件)
      */
     @Bean
     open fun nacosContextRefresher(nacosConfigManager: NacosConfigManager): NacosContextRefresher {
