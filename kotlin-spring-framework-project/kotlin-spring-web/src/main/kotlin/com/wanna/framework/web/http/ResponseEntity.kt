795aa4561d2bbb85f84a11202641dee5839e162b
jianchaojia <2801304373@qq.com>
2022-11-13 13:18:17 +0800

    完善

--- kotlin-spring-framework-project/kotlin-spring-web/src/main/kotlin/com/wanna/framework/web/http/ResponseEntity.kt
+++ kotlin-spring-framework-project/kotlin-spring-web/src/main/kotlin/com/wanna/framework/web/http/ResponseEntity.kt
@@ -1,5 +1,5 @@
 package com.wanna.framework.web.http
 
-class ResponseEntity<T>(val status: Any, headers: HttpHeaders, body: T?) : HttpEntity<T>(headers, body) {
+class ResponseEntity<T : Any>(val status: Any, headers: HttpHeaders, body: T?) : HttpEntity<T>(headers, body) {
 
 }
