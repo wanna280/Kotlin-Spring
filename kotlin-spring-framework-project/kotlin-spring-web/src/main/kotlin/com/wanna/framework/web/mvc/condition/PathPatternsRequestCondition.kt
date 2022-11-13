ba1ef145257f3c4b0c65da0ef53ce00a6bc90fa6
jianchaojia <2801304373@qq.com>
2022-11-14 01:42:33 +0800

    RequestMapping的path匹配的bugfix

--- kotlin-spring-framework-project/kotlin-spring-web/src/main/kotlin/com/wanna/framework/web/mvc/condition/PathPatternsRequestCondition.kt
+++ kotlin-spring-framework-project/kotlin-spring-web/src/main/kotlin/com/wanna/framework/web/mvc/condition/PathPatternsRequestCondition.kt
@@ -23,6 +23,11 @@ open class PathPatternsRequestCondition(private val patterns: Set<PathPattern>) 
         }
         val paths = HashSet<String>()
         other.paths.forEach { o -> this.paths.forEach { paths += "$o$it" } }
+
+        // bugfix: 如果方法上的@RequestMapping上没有配置路径的话, 那么我们需要沿用类上的@RequestMapping的路径
+        if (other.paths.isEmpty()) {
+            paths += this.paths
+        }
         return PathPatternsRequestCondition(*paths.toTypedArray())
     }
 
