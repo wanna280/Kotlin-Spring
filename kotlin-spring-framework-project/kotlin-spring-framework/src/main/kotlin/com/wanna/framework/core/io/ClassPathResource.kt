89ccbd45b2bb4bf8f3391888b2b5c295b5546631
jianchaojia <2801304373@qq.com>
2023-01-27 12:18:56 +0800

    ClassPathResource的描述信息完善

--- kotlin-spring-framework-project/kotlin-spring-framework/src/main/kotlin/com/wanna/framework/core/io/ClassPathResource.kt
+++ kotlin-spring-framework-project/kotlin-spring-framework/src/main/kotlin/com/wanna/framework/core/io/ClassPathResource.kt
@@ -11,28 +11,40 @@ import java.net.URL
 import java.nio.file.Files
 
 /**
- * ClassPath的资源, 基于类加载器去加载资源
+ * ClassPath的资源实现, 基于类加载器去加载资源
  *
  * @author jianchao.jia
  * @version v1.0
  * @date 2022/10/1
+ *
+ * @param path 资源路径
+ * @param classLoader 执行资源的加载ClassLoader
+ * @param clazz 执行资源的加载的类(将会使用该类的ClassLoader去进行加载)
  */
 open class ClassPathResource
 private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable private val clazz: Class<*>?) :
     AbstractFileResolvingResource(), WritableResource {
 
     /**
-     * 提供一个基于clazz的构建方法
+     * 提供一个基于Class的构建方法
+     *
+     * @param path 资源路径
+     * @param clazz 进行资源加载的类
      */
     constructor(path: String, clazz: Class<*>) : this(path, null, clazz)
 
     /**
      * 提供一个基于classLoader的构建方法
+     *
+     * @param path 资源路径
+     * @param classLoader 要去进行使用资源的加载时使用的类加载器(为null时将会使用默认的ClassLoader)
      */
     constructor(path: String, @Nullable classLoader: ClassLoader?) : this(path, classLoader, null)
 
     /**
-     * 提供一个只给定path的构造器
+     * 提供一个只给定path的构建方法, 使用默认的ClassLoader去进行加载
+     *
+     * @param path 资源路径
      */
     constructor(path: String) : this(path, null)
 
@@ -42,22 +54,26 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
     private val path = if (path.startsWith("/")) path.substring(1) else path
 
     /**
-     * 如果用户没有指定ClassLoader的话, 那么我们使用默认的ClassLoader
+     * 如果没有明确地去指定ClassLoader的话, 那么我们使用默认的ClassLoader进行资源加载
      */
     @Nullable
     private var classLoader: ClassLoader? = classLoader ?: ClassUtils.getDefaultClassLoader()
 
+    /**
+     * 获取ClassPath下的资源的[InputStream]
+     *
+     * @return InputStream of ClassPath Resource
+     */
     override fun getInputStream(): InputStream {
-        val stream =
-            if (clazz != null) {
-                clazz.getResourceAsStream(path)
-            } else if (classLoader != null) {
-                classLoader!!.getResourceAsStream(path)
-            } else {
-                ClassLoader.getSystemResourceAsStream(path)
-            }
+        val stream = if (clazz != null) {
+            clazz.getResourceAsStream(path)
+        } else if (classLoader != null) {
+            classLoader!!.getResourceAsStream(path)
+        } else {
+            ClassLoader.getSystemResourceAsStream(path)
+        }
         return stream
-            ?: throw FileNotFoundException("[${getDescription()}]资源无法解析成为InputStream, 因为该资源并不存在")
+            ?: throw FileNotFoundException("${getDescription()} cannot be opened because it does not exist")
     }
 
     /**
@@ -111,16 +127,7 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
      *
      * @return 当前ClassPath的资源的描述信息
      */
-    override fun getDescription(): String {
-        val builder = StringBuilder("class path resource [")
-        if (clazz != null) {
-            builder.append(clazz.name).append(".class")
-        } else {
-            builder.append(path)
-        }
-        builder.append("]")
-        return builder.toString()
-    }
+    override fun getDescription(): String = "class path resource [$path]"
 
     override fun isWritable(): Boolean {
         return try {
