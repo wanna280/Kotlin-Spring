ee4870850e2884592721a8e12d9964b8c4a7725d
jianchaojia <2801304373@qq.com>
2023-01-27 12:40:35 +0800

    ClassPathResource完善

--- kotlin-spring-framework-project/kotlin-spring-framework/src/main/kotlin/com/wanna/framework/core/io/ClassPathResource.kt
+++ kotlin-spring-framework-project/kotlin-spring-framework/src/main/kotlin/com/wanna/framework/core/io/ClassPathResource.kt
@@ -17,18 +17,20 @@ import java.nio.file.Files
  * @version v1.0
  * @date 2022/10/1
  *
- * @param path 资源路径
+ * @param path 要去进行加载的资源路径classpath
  * @param classLoader 执行资源的加载ClassLoader
- * @param clazz 执行资源的加载的类(将会使用该类的ClassLoader去进行加载)
+ * @param clazz 执行资源的加载的类(将会使用该类的ClassLoader去进行加载, 并且以该类的所在的包去作为baseDir, 去进行**相对路径**的资源寻找)
  */
-open class ClassPathResource
-private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable private val clazz: Class<*>?) :
-    AbstractFileResolvingResource(), WritableResource {
+open class ClassPathResource(
+    path: String,
+    @Nullable classLoader: ClassLoader?,
+    @Nullable private val clazz: Class<*>?
+) : AbstractFileResolvingResource(), WritableResource {
 
     /**
-     * 提供一个基于Class的构建方法
+     * 提供一个基于Class的构建方法, 资源加载时将会使用该类的ClassLoader去进行加载, 并且以该类的所在的包去作为baseDir, 去进行**相对路径**的资源寻找
      *
-     * @param path 资源路径
+     * @param path 资源路径classpath
      * @param clazz 进行资源加载的类
      */
     constructor(path: String, clazz: Class<*>) : this(path, null, clazz)
@@ -36,7 +38,7 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
     /**
      * 提供一个基于classLoader的构建方法
      *
-     * @param path 资源路径
+     * @param path 资源路径classpath
      * @param classLoader 要去进行使用资源的加载时使用的类加载器(为null时将会使用默认的ClassLoader)
      */
     constructor(path: String, @Nullable classLoader: ClassLoader?) : this(path, classLoader, null)
@@ -44,7 +46,7 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
     /**
      * 提供一个只给定path的构建方法, 使用默认的ClassLoader去进行加载
      *
-     * @param path 资源路径
+     * @param path 资源路径classpath
      */
     constructor(path: String) : this(path, null)
 
@@ -60,7 +62,7 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
     private var classLoader: ClassLoader? = classLoader ?: ClassUtils.getDefaultClassLoader()
 
     /**
-     * 获取ClassPath下的资源的[InputStream]
+     * 获取当前[ClassPathResource]资源的输入流[InputStream], 进行内容的读取
      *
      * @return InputStream of ClassPath Resource
      *
@@ -69,10 +71,15 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
      * @see ClassLoader.getSystemResourceAsStream
      */
     override fun getInputStream(): InputStream {
+        // 如果给定的是Class的话, 那么以Class所在的包的位置去作为baseDir去进行资源的寻找
         val stream = if (clazz != null) {
             clazz.getResourceAsStream(path)
+
+            // 如果给定的是ClassLoader的话, 那么将会尝试使用ClassLoader.getResourcesAsStream去进行资源的寻找
         } else if (classLoader != null) {
             classLoader!!.getResourceAsStream(path)
+
+            // 如果啥也没给的话, 那么将会使用ClassLoader.getSystemResourceAsStream去进行获取
         } else {
             ClassLoader.getSystemResourceAsStream(path)
         }
@@ -88,18 +95,18 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
     override fun exists(): Boolean = resolveURL() != null
 
     /**
-     * 获取到URL
+     * 获取到当前[ClassPathResource]的资源URL
      *
-     * @return URL of ClassPathResource
-     * @throws FileNotFoundException 如果该ClassPath的资源不存在
+     * @return URL of this ClassPathResource
+     * @throws FileNotFoundException 如果该ClassPathResource不存在
      */
     override fun getURL(): URL {
-        val url = resolveURL()
-        return url ?: throw FileNotFoundException("[${getDescription()}]资源无法被解析成为URL, 因为该资源并不存在")
+        return resolveURL()
+            ?: throw FileNotFoundException("${getDescription()} cannot be resolved as URL, because it does not exist")
     }
 
     /**
-     * 根据ClassLoader去resolveURL
+     * 解析得到当前[ClassPathResource]资源所在的[URL]
      *
      * @return URL(如果无法解析的话, return null)
      */
@@ -115,24 +122,30 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
     }
 
     /**
-     * 实现创建一个相对路径方法
+     * 创建一个以当前[Resource]作为基准路径的相对位置的[ClassPathResource]
      *
      * @param relativePath 相对当前资源的路径
-     * @return 根据相对路径解析到的资源
+     * @return 根据relativePath去解析到的[ClassPathResource]
      */
     override fun createRelative(relativePath: String): Resource {
+        // 根据给定的relativePath, 先去生成相对路径
         val pathToUse = ResourceUtils.applyRelativePath(path, relativePath)
+
+        // 创建ClassPathResource
         return if (clazz != null) ClassPathResource(pathToUse, clazz)
         else ClassPathResource(pathToUse, this.classLoader)
     }
 
     /**
-     * 获取资源的描述信息
+     * 获取当前[ClassPathResource]资源的描述信息
      *
      * @return 当前ClassPath的资源的描述信息
      */
     override fun getDescription(): String = "class path resource [$path]"
 
+    /**
+     * 当前[ClassPathResource]是否可写?
+     */
     override fun isWritable(): Boolean {
         return try {
             val file = getFile()
@@ -151,11 +164,11 @@ private constructor(path: String, @Nullable classLoader: ClassLoader?, @Nullable
      *
      * @param other other
      */
-    override fun equals(other: Any?): Boolean {
+    override fun equals(@Nullable other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ClassPathResource
+        if (other !is ClassPathResource) {
+            return false
+        }
 
         if (clazz != other.clazz) return false
         if (path != other.path) return false
