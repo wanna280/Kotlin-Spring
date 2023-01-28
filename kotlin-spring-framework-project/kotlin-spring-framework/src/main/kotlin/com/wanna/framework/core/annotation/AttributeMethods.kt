737d0444c97c5a90a23a0473a4efe4fc0b0791aa
jianchaojia <2801304373@qq.com>
2023-01-28 11:11:13 +0800

    AttributeMethods代码完善

--- kotlin-spring-framework-project/kotlin-spring-framework/src/main/kotlin/com/wanna/framework/core/annotation/AttributeMethods.kt
+++ kotlin-spring-framework-project/kotlin-spring-framework/src/main/kotlin/com/wanna/framework/core/annotation/AttributeMethods.kt
@@ -1,5 +1,6 @@
 package com.wanna.framework.core.annotation
 
+import com.wanna.framework.constants.CLASS_ARRAY_TYPE
 import com.wanna.framework.lang.Nullable
 import java.lang.reflect.Method
 import java.util.concurrent.ConcurrentHashMap
@@ -22,6 +23,127 @@ import java.util.concurrent.ConcurrentHashMap
 class AttributeMethods(
     @Nullable private val annotationType: Class<out Annotation>?, private val attributeMethods: Array<Method>
 ) {
+
+    /**
+     * 记录某个位置(index)的注解方法, 当依赖不存在时, 能否丢出来异常? 为true代表可以丢, 为false代表不能丢异常
+     * (对于Class[], Class, Enum这三种情况, 缺失的情况, 才需要丢出来异常)
+     */
+    private val canThrowTypeNotPresentException = BooleanArray(attributeMethods.size)
+
+    init {
+        for (index in attributeMethods.indices) {
+            val method = attributeMethods[index]
+            val returnType = method.returnType
+
+            // TODO
+
+            // 对于注解的属性类型, 是Class[], Class, Enum这三种情况, 依赖缺失的情况, 才需要丢出来异常
+            canThrowTypeNotPresentException[index] =
+                returnType == CLASS_ARRAY_TYPE || returnType == Class::class.java || returnType == Enum::class.java
+        }
+    }
+
+    /**
+     * 对注解去进行检验, 检验该注解当中定义的属性, 是否都是合法的?
+     * 因为很多时候, 有可能会遇到, 一个类上标注了A注解, 但是A注解的属性当中, 用到了一个类B,
+     * 但是类B很可能并不在我们的依赖当中, 此时就会产生[ClassNotFoundException]/[LinkageError],
+     * 但是很多时候, 我们允许去进行这样的配置, 我们很可能会继续尝试使用ASM去进行该注解当中的属性的读取
+     *
+     * @param annotation 待检验的注解对象
+     */
+    fun validate(annotation: Annotation) {
+        // first, 先检验一下Annotation和this.annotationType之间是否匹配?
+        assertAnnotation(annotation)
+
+        for (i in 0 until size) {
+            // 如果该位置, 就算缺了, 也不能丢出来异常的话, 那么直接pass掉
+            if (!canThrowTypeNotPresentException(i)) {
+                continue
+            }
+            try {
+                AnnotationUtils.invokeAnnotationMethod(attributeMethods[i], annotation)
+            } catch (ex: Throwable) {
+                throw IllegalStateException("Could not obtain annotation attribute value for '${get(i).name}' declared on '$annotationType'")
+            }
+        }
+    }
+
+    /**
+     * 从AttributeMethods当中根据name去找到合适的属性对应的index
+     *
+     * @param name 属性名
+     * @return 如果存在有这样的属性, 那么返回该属性所在的index; 如果不存在, 那么return -1
+     */
+    fun indexOf(name: String): Int {
+        attributeMethods.indices.forEach {
+            if (attributeMethods[it].name == name) {
+                return it
+            }
+        }
+        return -1
+    }
+
+    /**
+     * 从AttributeMethods当中, 去定位到给定的方法所在的index
+     *
+     * @param attribute attributeMethod
+     * @return 该方法所在的位置index, 不存在的话return -1
+     */
+    fun indexOf(attribute: Method): Int {
+        attributeMethods.indices.forEach {
+            if (attributeMethods[it] == attribute) {
+                return it
+            }
+        }
+        return -1
+    }
+
+    /**
+     * 从AttributeMethods当中, 根据name去定位到对应的属性方法
+     *
+     * @param name name
+     * @return 根据name获取到的属性方法(如果不存在的话, return null)
+     */
+    @Nullable
+    fun get(name: String): Method? {
+        val index = indexOf(name)
+        return if (index == -1) null else this[index]
+    }
+
+    /**
+     * 根据index去获取到对应的属性方法
+     *
+     * @param index index
+     * @return 该位置的属性方法
+     */
+    operator fun get(index: Int): Method = attributeMethods[index]
+
+    /**
+     * 属性方法的数量
+     */
+    val size: Int
+        get() = this.attributeMethods.size
+
+    /**
+     * 检验给定位置的注解属性, 当类不存在时, 能否丢出来异常?
+     *
+     * @param index 属性方法index
+     */
+    private fun canThrowTypeNotPresentException(index: Int): Boolean {
+        return true
+    }
+
+    /**
+     * 检验一下给定的注解对象, 和[annotationType]是否匹配?
+     *
+     * @param annotation 待检验的注解对象
+     */
+    private fun assertAnnotation(annotation: Annotation) {
+        if (annotationType != null && !annotationType.isInstance(annotation)) {
+            throw IllegalStateException("Object of class [${annotation::class.java.name}] must be an instance of $annotationType")
+        }
+    }
+
 
     companion object {
 
@@ -103,79 +225,5 @@ class AttributeMethods(
         private fun isAttributeMethod(method: Method): Boolean =
             method.parameterCount == 0 && method.returnType != Unit::class.java
     }
-
-    /**
-     * 对注解去进行检验, 检验该注解当中定义的属性, 是否都是合法的?
-     * 因为很多时候, 有可能会遇到, 一个类上标注了A注解, 但是A注解的属性当中, 用到了一个类B,
-     * 但是类B很可能并不在我们的依赖当中, 此时就会产生[ClassNotFoundException]/[LinkageError],
-     * 但是很多时候, 我们允许去进行这样的配置, 我们很可能会继续尝试使用ASM去进行该注解当中的属性的读取
-     *
-     * @param annotation 待检验的注解对象
-     */
-    fun validate(annotation: Annotation) {
-        for (i in 0 until size) {
-            try {
-                AnnotationUtils.invokeAnnotationMethod(attributeMethods[i], annotation)
-            } catch (ex: Throwable) {
-                throw IllegalStateException("Could not obtain annotation attribute value for '${get(i).name}' declared on '$annotationType'")
-            }
-        }
-    }
-
-    /**
-     * 从AttributeMethods当中根据name去找到合适的属性对应的index
-     *
-     * @param name 属性名
-     * @return 如果存在有这样的属性, 那么返回该属性所在的index; 如果不存在, 那么return -1
-     */
-    fun indexOf(name: String): Int {
-        attributeMethods.indices.forEach {
-            if (attributeMethods[it].name == name) {
-                return it
-            }
-        }
-        return -1
-    }
-
-    /**
-     * 从AttributeMethods当中, 去定位到给定的方法所在的index
-     *
-     * @param attribute attributeMethod
-     * @return 该方法所在的位置index, 不存在的话return -1
-     */
-    fun indexOf(attribute: Method): Int {
-        attributeMethods.indices.forEach {
-            if (attributeMethods[it] == attribute) {
-                return it
-            }
-        }
-        return -1
-    }
-
-    /**
-     * 从AttributeMethods当中, 根据name去定位到对应的属性方法
-     *
-     * @param name name
-     * @return 根据name获取到的属性方法(如果不存在的话, return null)
-     */
-    @Nullable
-    fun get(name: String): Method? {
-        val index = indexOf(name)
-        return if (index == -1) null else this[index]
-    }
-
-    /**
-     * 根据index去获取到对应的属性方法
-     *
-     * @param index index
-     * @return 该位置的属性方法
-     */
-    operator fun get(index: Int): Method = attributeMethods[index]
-
-    /**
-     * 属性方法的数量
-     */
-    val size: Int
-        get() = this.attributeMethods.size
 
 }
