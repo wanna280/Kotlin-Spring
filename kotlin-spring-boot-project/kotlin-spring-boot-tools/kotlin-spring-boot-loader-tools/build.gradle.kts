41dcc665602d7db39075aef5e0f8366d14465fbb
jianchaojia <2801304373@qq.com>
2023-02-06 04:21:31 +0800

    GradlePlugin代码完善

--- kotlin-spring-boot-project/kotlin-spring-boot-tools/kotlin-spring-boot-loader-tools/build.gradle.kts
+++ kotlin-spring-boot-project/kotlin-spring-boot-tools/kotlin-spring-boot-loader-tools/build.gradle.kts
@@ -2,29 +2,38 @@ dependencies {
     implementation("org.apache.commons:commons-compress:$commonCompressVersion")
 }
 
+// 生成的loader的jar包的存放位置
 val generatedResources = "$buildDir/generated-resources/main"
 
+// loader的项目路径
+val loaderProjectPath = ":kotlin-spring-boot-project:kotlin-spring-boot-tools:kotlin-spring-boot-loader"
 
-
-// 将spring-boot-loader去打包一下(目前做法不太行, 待优化)
+// 将spring-boot-loader工程去进行打包成为Jar, 并放入到当前Jar包的"META-INF/loader"下
 tasks.create<Jar>("reproducibleLoaderJar") {
-    val loaderProject = project(":kotlin-spring-boot-project:kotlin-spring-boot-tools:kotlin-spring-boot-loader")
+    // 获取到Loader工程的Jar任务
+    val loaderJarTask = tasks.getByPath("$loaderProjectPath:jar") as Jar
 
-    // zipFile, 用于将归档文件去进行展开...
-    from(zipTree(loaderProject.buildDir.absolutePath + "/libs/kotlin-spring-boot-loader-${project.version}.jar")) {
+    // 当前任务依赖于Loader工程的Jar任务, 等它执行完成之后, 这个任务再去进行构建
+    this.dependsOn(loaderJarTask)
+
+    // 在执行当前工程的Jar任务之前, 先去执行当前任务, 因为生成Jar包需要用到当前的任务
+    tasks.getByName<Jar>("jar").dependsOn(this)
+
+    // zipTree方法, 用于将归档文件去进行展开...
+    // 将loader的工程的产物, 全部copy过来, 去生成一个新的Archive产物
+    from(zipTree(loaderJarTask.archiveFile.get())) {
         include("**/**")
     }
 
-    tasks.getByName<Jar>("jar").dependsOn(this)
-
-    // 要去进行构建的Archive归档文件的文件名?
+    // 要去进行构建的Archive归档文件的文件名为"kotlin-spring-boot-loader.jar"
     this.archiveFileName.set("kotlin-spring-boot-loader.jar")
 
-    // 要将构建出来的Archive归档文件放到哪里?
+    // 要将构建出来的Archive归档文件放到"{generatedResource}/META-INF/loader"下, 这样最终就会将这个Jar包放到到META-INF/loader/目录下
     destinationDirectory.set(file("$generatedResources/META-INF/loader"))
 }
 
 sourceSets {
+    // 把generatedResources去添加到main的SourceSet当中, 这样打包的时候, 就能自动将该Jar包去进行copy
     getByName("main") {
         this.output.dir(generatedResources)
     }
