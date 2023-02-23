e3cc553967ee42d74daa903389e23baf0feca69e
wanna280 <63626655+wanna280@users.noreply.github.com>
2023-02-24 00:38:29 +0800

    core代码完善

--- kotlin-spring-framework-project/kotlin-spring-core/src/main/kotlin/com/wanna/framework/core/type/classreading/MetadataReaderFactory.kt
+++ kotlin-spring-framework-project/kotlin-spring-core/src/main/kotlin/com/wanna/framework/core/type/classreading/MetadataReaderFactory.kt
@@ -3,18 +3,20 @@ package com.wanna.framework.core.type.classreading
 import com.wanna.framework.core.io.Resource
 
 /**
- * 快速构建MetadataReader的工厂
+ * 快速构建[MetadataReader]的工厂
  *
  * @author jianchao.jia
  * @version v1.0
  * @date 2022/12/18
  *
  * @see MetadataReader
+ * @see SimpleMetadataReaderFactory
+ * @see CachingMetadataReaderFactory
  */
 interface MetadataReaderFactory {
 
     /**
-     * 根据给定的类名, 去快速创建读取该类的元信息的MetadataReader
+     * 根据给定的类名, 去快速创建读取该类的元信息的[MetadataReader]
      *
      * @param className className
      * @return 读取该类的Class文件的读取的的MetadataReader
@@ -22,7 +24,7 @@ interface MetadataReaderFactory {
     fun getMetadataReader(className: String): MetadataReader
 
     /**
-     * 根据给定的Resource, 去快速构建该类的元信息的MetadataReader
+     * 根据给定的[Resource], 去快速构建该类的元信息的[MetadataReader]
      *
      * @param resource Class文件的Resource
      * @return 该Resource对应Class文件的读取的MetadataReader
