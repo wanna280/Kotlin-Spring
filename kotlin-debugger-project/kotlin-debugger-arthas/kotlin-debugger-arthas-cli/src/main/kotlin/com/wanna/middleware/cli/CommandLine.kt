6b82e79946726e1b6a2e498851d61366330a5f24
jianchaojia <2801304373@qq.com>
2023-03-12 23:58:33 +0800

    arthas cli工程注释修正&完善

--- kotlin-debugger-project/kotlin-debugger-arthas/kotlin-debugger-arthas-cli/src/main/kotlin/com/wanna/middleware/cli/CommandLine.kt
+++ kotlin-debugger-project/kotlin-debugger-arthas/kotlin-debugger-arthas-cli/src/main/kotlin/com/wanna/middleware/cli/CommandLine.kt
@@ -5,6 +5,18 @@ import javax.annotation.Nullable
 /**
  * 对于单个命令行参数列表的解析的结果
  *
+ * 对于一个命令行来说, 分为name/option/argument三个部分
+ *
+ * * 1.对于name, 也就是命令的名字, 对应的就是本地的二进制可执行文件.
+ *
+ * * 2.对于Option, 通常以'-'或者是'--'作为开头, 后面跟一个选项名称.
+ * Option通常情况下, 都会有一个默认值, 因此不指定Option时将会使用默认值.
+ * Option通常情况下, 只会有Boolean类型.
+ *
+ * * 3.对于Argument, 用于传递参数信息, 通常情况下没有默认值, Argument
+ * 通常情况下都要求用户必须指定,如果不指定的话, 将会提示用户去进行输入.
+ * Argument通常情况下, 可以是字符串/数字/文件路径等不同类型的数据.
+ *
  * @author jianchao.jia
  * @version v1.0
  * @date 2023/2/25
